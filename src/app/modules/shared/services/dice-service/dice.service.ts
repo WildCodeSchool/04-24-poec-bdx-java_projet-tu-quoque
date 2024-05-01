9f7545885a2e2be943a4c9f845725bc77c0f6727
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-01 21:07:57 +0200

    feat: add parsing string to PossibleDice

--- src/app/modules/shared/services/dice-service/dice.service.ts
+++ src/app/modules/shared/services/dice-service/dice.service.ts
@@ -28,4 +28,25 @@ export class DiceService {
       .slice(1)
       .reduce((accum, e) => accum + e, 0);
   }
+
+  transformStringToDice(dice: string): PossibleDice {
+    switch (dice) {
+      case "d3":
+        return PossibleDice.d3;
+      case "d4":
+        return PossibleDice.d4;
+      case "d6":
+        return PossibleDice.d6;
+      case "d8":
+        return PossibleDice.d8;
+      case "d10":
+        return PossibleDice.d10;
+      case "d12":
+        return PossibleDice.d12;
+      case "d20":
+        return PossibleDice.d20;
+      default:
+        throw new Error("dice not recognized");
+    }
+  }
 }
