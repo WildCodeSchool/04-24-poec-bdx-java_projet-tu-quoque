d19066b5019da411459c435957cef56441598834
bfsylvain <sylvainbonnaure@hotmail.com>
2024-05-05 18:29:39 +0200

    feature: shared component 'list of elements' created

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -24,7 +24,7 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
     HttpClientModule
 
   ],
