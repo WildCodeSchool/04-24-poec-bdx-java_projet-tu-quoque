cd0fa61e1576ba518ec0c085a42b009c2716c103
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-05 21:37:36 +0200

    feat: inject db-service

--- src/app/modules/character-sheet/pages/sheet-page/sheet-page.component.ts
+++ src/app/modules/character-sheet/pages/sheet-page/sheet-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DbService } from '../../../shared/services/db-service/db.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sheet-page',
   templateUrl: './sheet-page.component.html',
   styleUrl: './sheet-page.component.scss'
 })
-export class SheetPageComponent {
+export class SheetPageComponent implements OnInit {
+  raceList$!: Observable<any>;
+
+  constructor(private dbService: DbService) {
+
+  }
+
+  ngOnInit(): void {
+    this.raceList$ = this.dbService.getRaces$();
+  }
 
 }
