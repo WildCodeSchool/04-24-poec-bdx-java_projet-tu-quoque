5b776f3a4e830702b66bba76402e386531db389c
bfsylvain <sylvainbonnaure@hotmail.com>
2024-05-06 20:05:36 +0200

    style: app-routing module indentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ const routes: Routes = [
 	{ path: 'character-sheet', loadChildren: () =>  import('./modules/character-sheet/character-sheet.module').then(m  => m.CharacterSheetModule) },
   {
     path: 'authentification', 
-    loadChildren: () =>  import('./modules/authentification/authentification.module').then(m  => m.AuthentificationModule) 
+    loadChildren: () => import('./modules/authentification/authentification.module')
+    .then(m  => m.AuthentificationModule) 
   },
   {
     path: 'user', 
-    loadChildren: () =>  import('./modules/user/user.module').then(m  => m.UserModule) 
+    loadChildren: () => import('./modules/user/user.module')
+    .then(m  => m.UserModule) 
   }
 ];
 
