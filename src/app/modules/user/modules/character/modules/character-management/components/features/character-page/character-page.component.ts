d7d3d9ac7bceac70441ed53fd82a6daee518371c
bfsylvain <sylvainbonnaure@hotmail.com>
2024-05-06 11:35:35 +0200

    feature: character page component created

---
+++ src/app/modules/user/modules/character/modules/character-management/components/features/character-page/character-page.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CharacterService } from '../../../../../../../../shared/services/character/character.service';
+import { Observable, map } from 'rxjs';
+
+@Component({
+  selector: 'app-character-page',
+  templateUrl: './character-page.component.html',
+  styleUrl: './character-page.component.scss',
+})
+export class CharacterPageComponent implements OnInit {
+  
+  character$!: Observable<any>;
+  characterDiscussionList: any = [
+    {
+      id: 1,
+      name: 'Discussion 1',
+    },
+    {
+      id: 2,
+      name: 'Discussion 2',
+    },
+    {
+      id: 3,
+      name: 'Discussion 3',
+    },
+    {
+      id: 4,
+      name: 'Discussion 4',
+    },
+  ];
+
+  constructor(
+    private _characterService: CharacterService,
+    private _route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this._route.snapshot.paramMap.get('id'));
+    this.character$ = this._characterService.getCharacterById(id);
+  }
+}
