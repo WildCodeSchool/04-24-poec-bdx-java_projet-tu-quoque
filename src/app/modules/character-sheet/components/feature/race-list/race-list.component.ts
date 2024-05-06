72a515edc3c1b04b6b71635b47847e9618f245ce
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-06 21:33:48 +0200

    feat: add race list component

---
+++ src/app/modules/character-sheet/components/feature/race-list/race-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Race } from '../../../models/types/race.type';
+import { Observable } from 'rxjs';
+import { DbService } from '../../../../shared/services/db-service/db.service';
+
+@Component({
+  selector: 'app-race-list',
+  templateUrl: './race-list.component.html',
+  styleUrl: './race-list.component.scss'
+})
+export class RaceListComponent {
+  raceList$!: Observable<Race[]>;
+  selectName: string = "characterClass";
+  selectLabel: string = "CLASSE";
+
+  constructor(private raceService: DbService) {
+
+  }
+
+  ngOnInit(): void {
+    this.raceList$ = this.raceService.getRaces$();
+  }
+}
