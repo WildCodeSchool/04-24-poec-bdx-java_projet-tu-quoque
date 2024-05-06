85b5ca7b8d084fcdc242af4e6677eba930d6cb11
bfsylvain <sylvainbonnaure@hotmail.com>
2024-05-06 20:07:18 +0200

    feature: user tables component created

---
+++ src/app/modules/user/modules/table/modules/table-management/components/features/user-tables/user-tables.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-tables',
+  templateUrl: './user-tables.component.html',
+  styleUrl: './user-tables.component.scss'
+})
+export class UserTablesComponent {
+
+}
