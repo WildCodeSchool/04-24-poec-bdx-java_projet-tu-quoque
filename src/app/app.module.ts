44aeb0e092db8813ed6464d2114afe0b9a5ee513
Nelia <nelia.courtais@live.fr>
2024-05-06 15:18:01 +0200

    feature: add custom-form component + connexion-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavigationModule } from './modules/navigation/navigation.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AuthentificationModule } from './modules/authentification/authentification.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { FormsModule } from '@angular/forms';
     ButtonModule,
     NavigationModule,
     AuthentificationModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomButtonComponent } from './components/custom-button/custom-button.component';
-import { CustomFormComponent } from './components/custom-form/custom-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InputErrorComponent } from './components/custom-form/errors/input-error/input-error.component';
 
 
 
 @NgModule({
   declarations: [
     CustomButtonComponent,
-    CustomFormComponent
   ],
   imports: [
     CommonModule,
@@ -18,7 +17,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   ],
   exports: [
     CustomButtonComponent,
-    CustomFormComponent
   ]
 })
 export class SharedModule { }
