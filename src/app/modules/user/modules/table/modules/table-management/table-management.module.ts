8c87219dc638220eae103b78b7fbdab1b92ebb86
bfsylvain <sylvainbonnaure@hotmail.com>
2024-05-07 20:14:08 +0200

    feature: table page + routing + service

--- src/app/modules/user/modules/table/modules/table-management/table-management.module.ts
+++ src/app/modules/user/modules/table/modules/table-management/table-management.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { TableManagementRoutingModule } from './table-management-routing.module';
 import { UserTablesComponent } from './components/features/user-tables/user-tables.component';
 import { SharedModule } from '../../../../../shared/shared.module';
+import { TablePageComponent } from './components/features/table-page/table-page.component';
+import { TablePresentationComponent } from './components/ui/table-presentation/table-presentation.component';
 
 
 @NgModule({
   declarations: [
-    UserTablesComponent
+    UserTablesComponent,
+    TablePageComponent,
+    TablePresentationComponent
   ],
   imports: [
     CommonModule,
