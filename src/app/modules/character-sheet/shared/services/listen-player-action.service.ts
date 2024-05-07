b1a95ca1cb4dd1693d72adcc6e22109d0550b0ef
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-07 08:48:43 +0200

    feat: add listen service
    a service to listen player's entries on the character sheet

---
+++ src/app/modules/character-sheet/shared/services/listen-player-action.service.ts
@@ -0,0 +1,27 @@
+import { DestroyRef, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Field } from '../models/types/field.type';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListenPlayerActionService {
+  sheetModifiedByPlayer: any = {};
+  private sheetModifiedListener$: BehaviorSubject<any> = new BehaviorSubject(this.sheetModifiedByPlayer);
+
+  constructor(private destroyRef: DestroyRef) { }
+
+  receiveInfoFrom(fromObs$: Observable<Field>): void {
+    fromObs$.pipe(
+      takeUntilDestroyed(this.destroyRef),
+    ).subscribe((field: Field) => {
+      this.sheetModifiedByPlayer[field.name] = field.value;
+      this.sheetModifiedListener$.next(this.sheetModifiedByPlayer);
+    });
+  }
+
+  sendInfos() {
+    return this.sheetModifiedListener$.asObservable();
+  }
+}
