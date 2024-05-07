74d7f5e7c4b802a885d9287cf1c964758aaf0d33
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-07 08:46:27 +0200

    feat: add gender list component

---
+++ src/app/modules/character-sheet/components/feature/gender-list/gender-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DbService } from '../../../../shared/services/db-service/db.service';
+import { Gender } from '../../../models/types/gender.type';
+
+@Component({
+  selector: 'app-gender-list',
+  templateUrl: './gender-list.component.html',
+  styleUrl: './gender-list.component.scss'
+})
+export class GenderListComponent {
+  list$!: Observable<Gender[]>;
+  selectName: string = "gender";
+  selectLabel: string = "SEXE";
+
+  constructor(private dbService: DbService) {
+
+  }
+
+  ngOnInit(): void {
+    this.list$ = this.dbService.getGenders$();
+  }
+}
