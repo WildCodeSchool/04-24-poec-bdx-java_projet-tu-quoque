86037be9db3f832eb506814069fda53e83abfb5b
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-07 08:43:55 +0200

    feat: add gentGender$

--- src/app/modules/shared/services/db-service/db.service.ts
+++ src/app/modules/shared/services/db-service/db.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Race } from '../../../character-sheet/models/types/race.type';
 import { CharacterClass } from '../../../character-sheet/models/types/character-class.type';
 import { Alignment } from '../../../character-sheet/models/types/alignment.type';
+import { Gender } from '../../../character-sheet/models/types/gender.type';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,7 @@ export class DbService {
   private readonly RACES_ENDPOINT: string = "races";
   private readonly CLASSES_ENDPOINT: string = "classes";
   private readonly ALIGNMENTS_ENDPOINT: string = "alignments";
+  private readonly GENDERS_ENDPOINT: string = "genders";
 
   constructor(private http: HttpClient) { }
 
@@ -31,4 +33,10 @@ export class DbService {
   getAlignments$(): Observable<Alignment[]> {
     return this.getEndpoint$(this.ALIGNMENTS_ENDPOINT);
   }
+
+  getGenders$(): Observable<Gender[]> {
+    return this.getEndpoint$(this.GENDERS_ENDPOINT);
+  }
+
+
 }
