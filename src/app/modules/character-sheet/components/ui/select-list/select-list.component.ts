0f30c08e6cfa28b28604b89c58ef27384fdfc07f
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-07 08:50:47 +0200

    feat: includes listener service
    now select-list can send info when player changes something

--- src/app/modules/character-sheet/components/ui/select-list/select-list.component.ts
+++ src/app/modules/character-sheet/components/ui/select-list/select-list.component.ts
@@ -1,16 +1,37 @@
-import { Component, Input } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { ListenPlayerActionService } from '../../../shared/services/listen-player-action.service';
+import { Field } from '../../../shared/models/types/field.type';
 
 @Component({
   selector: 'app-select-list',
   templateUrl: './select-list.component.html',
   styleUrl: './select-list.component.scss'
 })
-export class SelectListComponent {
+export class SelectListComponent implements OnInit {
   @Input()
   list$!: Observable<any>;
   @Input()
   selectName!: string;
   @Input()
   selectLabel!: string;
+
+  playerChoice$: Subject<Field> = new Subject();
+
+  constructor(private listener: ListenPlayerActionService) {
+
+  }
+
+  ngOnInit(): void {
+    this.listener.receiveInfoFrom(this.playerChoice$);
+  }
+
+  sendChanges(value: string) {
+    const field: Field = {
+      name: this.selectName,
+      value: value
+    }
+    this.playerChoice$.next(field);
+  }
+
 }
