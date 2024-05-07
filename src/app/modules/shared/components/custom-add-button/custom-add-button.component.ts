7e742f3521076a2e463a4a171be469dedfea8829
bfsylvain <sylvainbonnaure@hotmail.com>
2024-05-07 16:18:18 +0200

    feat: custom-add-button creation

---
+++ src/app/modules/shared/components/custom-add-button/custom-add-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-add-button',
+  templateUrl: './custom-add-button.component.html',
+  styleUrl: './custom-add-button.component.scss'
+})
+export class CustomAddButtonComponent {
+
+  @Input()
+  style!: string
+}
