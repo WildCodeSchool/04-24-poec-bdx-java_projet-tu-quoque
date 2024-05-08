6a38f4a07e842c765602820c7db357a745fcfd82
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-08 16:16:59 +0200

    feat: add height-auto
    search, display and add to sheet height, in function of race, gender and diceroll

---
+++ src/app/modules/character-sheet/components/feature/height-auto/height-auto.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Race } from '../../../models/types/race.type';
+import { distinctUntilChanged, map, Observable } from 'rxjs';
+import { CharacterSheetService } from '../../../shared/services/character-sheet.service';
+import { CharacterClass } from '../../../models/types/character-class.type';
+import { ListenPlayerActionService } from '../../../shared/services/listen-player-action.service';
+
+@Component({
+  selector: 'app-height-auto',
+  templateUrl: './height-auto.component.html',
+  styleUrl: './height-auto.component.scss'
+})
+export class HeightAutoComponent implements OnInit {
+  height$: Observable<string> = this.characterSheetService.setHeight$();
+
+  constructor(
+    private characterSheetService: CharacterSheetService,
+    private listener: ListenPlayerActionService
+  ) {
+  }
+
+  ngOnInit() {
+    //this.race$.subscribe(race => console.log(race));
+    //this.characterSheetService.setSizeCategory$();
+    this.listener.receiveInfoFrom(this.height$.pipe(
+      distinctUntilChanged(),
+      map(heightValue => {
+        return { name: "height", value: heightValue }
+      })
+    ));
+  }
+}
+
