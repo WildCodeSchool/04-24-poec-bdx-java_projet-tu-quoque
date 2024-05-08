085b73c91edcf70076d88011dfa068b45952ad94
Nelia <nelia.courtais@live.fr>
2024-05-08 15:08:57 +0200

    feature : add new character text input

--- src/app/modules/user/modules/character/modules/character-creation/character-creation.module.ts
+++ src/app/modules/user/modules/character/modules/character-creation/character-creation.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CharacterCreationRoutingModule } from './character-creation-routing.module';
-import { NewCharacterComponent } from './components/features/new-character/new-character.component';
 import { SharedModule } from '../../../../../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    NewCharacterComponent
+
   ],
   imports: [
     CommonModule,
