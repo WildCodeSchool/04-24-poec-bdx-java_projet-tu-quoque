f39a9a41638278a9e9e108020636778615c89b6f
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-08 16:04:25 +0200

    feat: add age-medifier-per-race type

---
+++ src/app/modules/character-sheet/models/types/age-modifier-per-race.type.ts
@@ -0,0 +1,13 @@
+export type AgeModifierPerRace = {
+    Barbare: string,
+    Ensorceleur: string,
+    Roublard: string,
+    Barde: string,
+    Guerrier: string,
+    Paladin: string,
+    Rôdeur: string,
+    Druide: string,
+    Magicien: string,
+    Moine: string,
+    Prêtre: string
+}
