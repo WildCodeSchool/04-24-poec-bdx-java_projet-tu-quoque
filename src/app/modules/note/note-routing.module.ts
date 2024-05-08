7b8b730d23b20c9a6a1a7833590d9f3956ea1f5d
bfsylvain <sylvainbonnaure@hotmail.com>
2024-05-08 20:19:48 +0200

    note module creation

---
+++ src/app/modules/note/note-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserNotesComponent } from './components/features/user-notes/user-notes.component';
+import { GameNotesComponent } from './components/features/game-notes/game-notes.component';
+
+const routes: Routes = [
+  {
+    path:"",
+    redirectTo: "userNotes/:id",
+    pathMatch: "full"
+  },
+  {
+    path: "userNotes/:id",
+    component: UserNotesComponent
+  },
+  {
+    path: "characterNotes/:id",
+    component: GameNotesComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class NoteRoutingModule { }
