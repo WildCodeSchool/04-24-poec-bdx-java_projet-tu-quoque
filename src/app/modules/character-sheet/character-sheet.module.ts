e3ed36f4a8bf3458cc93d40c8e8fa12fe146e825
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-09 19:14:18 +0200

    refactor: rename components directory to character-informations-components

--- src/app/modules/character-sheet/character-sheet.module.ts
+++ src/app/modules/character-sheet/character-sheet.module.ts
@@ -3,23 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { CharacterSheetRoutingModule } from './character-sheet-routing.module';
 import { SheetPageComponent } from './pages/sheet-page/sheet-page.component';
-import { CharacterInformationsComponent } from './components/ui/character-informations/character-informations.component';
-import { ClassesListComponent } from './components/feature/lists/classes-list/classes-list.component';
-import { RaceListComponent } from './components/feature/lists/race-list/race-list.component';
-import { SelectListComponent } from './components/ui/select-list/select-list.component';
-import { AlignmentListComponent } from './components/feature/lists/alignment-list/alignment-list.component';
+import { CharacterInformationsComponent } from './character-informations-components/ui/character-informations/character-informations.component';
+import { ClassesListComponent } from './character-informations-components/feature/lists/classes-list/classes-list.component';
+import { RaceListComponent } from './character-informations-components/feature/lists/race-list/race-list.component';
+import { SelectListComponent } from './character-informations-components/ui/select-list/select-list.component';
+import { AlignmentListComponent } from './character-informations-components/feature/lists/alignment-list/alignment-list.component';
 import { FormsModule } from '@angular/forms';
 import { NgModelDebounceChangeDirective } from '../shared/directives/ng-model-debounce-change.directive';
-import { GenderListComponent } from './components/feature/lists/gender-list/gender-list.component';
-import { SizeCategorieAutoComponent } from './components/feature/auto/size-categorie-auto/size-categorie-auto.component';
-import { HeightAutoComponent } from './components/feature/auto/height-auto/height-auto.component';
-import { SelfFilledComponent } from './components/ui/self-filled/self-filled.component';
-import { WeightAutoComponent } from './components/feature/auto/weight-auto/weight-auto.component';
-import { AgeAutoComponent } from './components/feature/auto/age-auto/age-auto.component';
-import { InputFieldComponent } from './components/ui/input-field/input-field.component';
-import { FirstLineInputsComponent } from './components/feature/inputs/first-line-inputs/first-line-inputs.component';
-import { GodInputComponent } from './components/feature/inputs/god-input/god-input.component';
-import { ThirdLineInputsComponent } from './components/feature/inputs/third-line-inputs/third-line-inputs.component';
+import { GenderListComponent } from './character-informations-components/feature/lists/gender-list/gender-list.component';
+import { SizeCategorieAutoComponent } from './character-informations-components/feature/auto/size-categorie-auto/size-categorie-auto.component';
+import { HeightAutoComponent } from './character-informations-components/feature/auto/height-auto/height-auto.component';
+import { SelfFilledComponent } from './character-informations-components/ui/self-filled/self-filled.component';
+import { WeightAutoComponent } from './character-informations-components/feature/auto/weight-auto/weight-auto.component';
+import { AgeAutoComponent } from './character-informations-components/feature/auto/age-auto/age-auto.component';
+import { InputFieldComponent } from './character-informations-components/ui/input-field/input-field.component';
+import { FirstLineInputsComponent } from './character-informations-components/feature/inputs/first-line-inputs/first-line-inputs.component';
+import { GodInputComponent } from './character-informations-components/feature/inputs/god-input/god-input.component';
+import { ThirdLineInputsComponent } from './character-informations-components/feature/inputs/third-line-inputs/third-line-inputs.component';
 
 
 @NgModule({
