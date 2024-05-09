a4537e02717c6ca9414a30dbcd4ed2de48e9ff8a
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-09 21:04:11 +0200

    refactor: place character-informations-components in components directory

---
+++ src/app/modules/character-sheet/components/character-informations-components/feature/auto/weight-auto/weight-auto.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { distinctUntilChanged, map, Observable } from 'rxjs';
+import { CharacterSheetService } from '../../../../../shared/services/character-sheet.service';
+import { ListenPlayerActionService } from '../../../../../shared/services/listen-player-action.service';
+
+@Component({
+  selector: 'app-weight-auto',
+  templateUrl: './weight-auto.component.html',
+  styleUrl: './weight-auto.component.scss'
+})
+export class WeightAutoComponent {
+  weight$: Observable<string> = this.characterSheetService.setWeight$();
+  unit: string = "kgs";
+  label: string = "POIDS";
+
+  constructor(
+    private characterSheetService: CharacterSheetService,
+    private listener: ListenPlayerActionService
+  ) {
+  }
+
+  ngOnInit() {
+    this.listener.receiveInfoFrom(this.weight$.pipe(
+      distinctUntilChanged(),
+      map(weightValue => {
+        return { name: "weight", value: weightValue }
+      })
+    ));
+  }
+}
---
+++ src/app/modules/character-sheet/components/character-informations-components/feature/lists/alignment-list/alignment-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DbService } from '../../../../../../shared/services/db-service/db.service';
+import { Alignment } from '../../../../../models/types/alignment.type';
+
+@Component({
+  selector: 'app-alignment-list',
+  templateUrl: './alignment-list.component.html',
+  styleUrl: './alignment-list.component.scss'
+})
+export class AlignmentListComponent {
+
+  list$: Observable<Alignment[]> = this.dbService.getAlignments$();
+  selectName: string = "alignment";
+  selectLabel: string = "ALIGNEMENT";
+
+  constructor(private dbService: DbService) {
+  }
+}
