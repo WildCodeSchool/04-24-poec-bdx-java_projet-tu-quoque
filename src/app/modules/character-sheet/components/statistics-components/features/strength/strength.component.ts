f784b55a852a624c0bb65ec7523f6a33eb2e0881
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-09 21:10:36 +0200

    feat: add a component to manage strength (empty for the moment)

---
+++ src/app/modules/character-sheet/components/statistics-components/features/strength/strength.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-strength',
+  templateUrl: './strength.component.html',
+  styleUrl: './strength.component.scss'
+})
+export class StrengthComponent {
+
+}
