e6c3f3bd214fc0bc378d8def48b63834ee72525a
bfsylvain <sylvainbonnaure@hotmail.com>
2024-05-13 20:23:06 +0200

    feat: table-invitation service created + user hompage modified

---
+++ src/app/modules/shared/services/table-invitation/table-invitation.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TableInvitationService {
+
+  private _userId = 1;
+  private readonly _BASE_URL = "http://localhost:3000/user_table_invitations";
+  userTableInvitationList$: BehaviorSubject<any> = new BehaviorSubject([])
+
+  constructor(private _HTTP: HttpClient) { }
+
+  getTableInvitationList$(): Observable<any> {
+    return this._HTTP.get("http://localhost:3000/user_table_invitations")
+  }
+
+  getTableInvitationListByUser$(id: number): Observable<any> {
+    return this.getTableInvitationList$()
+    .pipe(
+      map((tableInvitationList: any) => tableInvitationList.filter((invitation : any) => Number(invitation.user_id) === id)))
+  }
+
+  getUserTableInvitationList$(): Observable<any> {
+    return this.userTableInvitationList$.value.length ? 
+      this.userTableInvitationList$.asObservable() 
+      : this.getTableInvitationListByUser$(this._userId)
+  }
+}
