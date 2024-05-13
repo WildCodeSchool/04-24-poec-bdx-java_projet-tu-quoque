055609144b0628733905b431e977f9224f1a348d
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-13 16:17:12 +0200

    feat: add a method to get skills from db

--- src/app/modules/shared/services/db-service/db.service.ts
+++ src/app/modules/shared/services/db-service/db.service.ts
@@ -5,6 +5,7 @@ import { Race } from '../../../character-sheet/models/types/race.type';
 import { CharacterClass } from '../../../character-sheet/models/types/character-class.type';
 import { Alignment } from '../../../character-sheet/models/types/alignment.type';
 import { Gender } from '../../../character-sheet/models/types/gender.type';
+import { SkillFromDb } from '../../../character-sheet/models/types/skill-from-db.type';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,7 @@ export class DbService {
   private readonly CLASSES_ENDPOINT: string = "classes";
   private readonly ALIGNMENTS_ENDPOINT: string = "alignments";
   private readonly GENDERS_ENDPOINT: string = "genders";
+  private readonly SKILLS_ENDPOINT: string = "skills";
 
   private race$: Observable<Race[]>;
 
@@ -43,6 +45,10 @@ export class DbService {
 
   getGenders$(): Observable<Gender[]> {
     return this.getEndpoint$(this.GENDERS_ENDPOINT);
+  }
+
+  getSkills$(): Observable<SkillFromDb[]> {
+    return this.getEndpoint$(this.SKILLS_ENDPOINT);
   }
 
 
