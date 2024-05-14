255b84a9c21d4c1e91126f5003feef9342c58737
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-14 21:27:25 +0200

    feat: add a list for levels

---
+++ src/app/modules/character-sheet/components/character-informations-components/feature/lists/level-list/level-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { from, Observable, of } from 'rxjs';
+import { Level } from '../../../../../models/types/level.type';
+
+@Component({
+  selector: 'app-level-list',
+  templateUrl: './level-list.component.html',
+  styleUrl: './level-list.component.scss'
+})
+export class LevelListComponent {
+  list$: Observable<Level[]> = of(Array.from({ length: 20 }, (x, i) => { return { name: i + 1 } }));
+  selectName: string = "level";
+  selectLabel: string = "NIVEAU";
+}
