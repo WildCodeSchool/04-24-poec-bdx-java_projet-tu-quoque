892801f4ca3774ef8d00a3b23d2ba7089c1e62a6
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-18 12:32:47 +0200

    refactor: change profoundly the structure of listenPlayerAction

--- src/app/modules/character-sheet/shared/services/listen-player-action.service.ts
+++ src/app/modules/character-sheet/shared/services/listen-player-action.service.ts
@@ -1,10 +1,13 @@
 import { DestroyRef, Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { Field } from '../models/types/field.type';
+import { BasicField } from '../models/types/basic-field.type';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { StatisticDetails } from '../../models/classes/statistic-details.class';
 import { StatField } from '../models/types/stat-field.type';
 import { SkillDetails } from '../../models/classes/skill-details.class';
+import { Field } from '../models/types/field.type';
+import { SkillField } from '../models/types/skill-field.type';
+import { StatListField } from '../models/types/stat-list-field.type';
 
 @Injectable({
   providedIn: 'root'
@@ -15,55 +18,64 @@ export class ListenPlayerActionService {
 
   constructor(private destroyRef: DestroyRef) { }
 
-  receiveInfoFrom(fromObs$: Observable<Field>): void {
-    fromObs$.pipe(
-      takeUntilDestroyed(this.destroyRef),
-    ).subscribe((field: Field) => {
-      this.controlField(field);
-      this.sheetModifiedByPlayer[field.name] = field.value;
-      this.sheetModifiedListener$.next(this.sheetModifiedByPlayer);
-    });
-  }
-
   sendInfos() {
     return this.sheetModifiedListener$.asObservable();
   }
 
-  controlField(field: Field) {
-    if (['characterRace', 'gender'].includes(field.name)) {
+  receiveFieldFrom(fromObs$: Observable<Field>) {
+    fromObs$.pipe(
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe((field: Field) => {
+      if (field.value instanceof SkillDetails) {
+        this.receiveSkillField(field as SkillField);
+      } else if (field.value instanceof StatisticDetails) {
+        this.receiveStatField(field as StatField);
+      } else if (field.value instanceof Array) {
+        this.receiveStatListField(field as StatListField)
+      } else {
+        this.receiveBasicField(field as BasicField)
+      }
+    })
+  }
+
+  controlField(field: BasicField) {
+    if (['characterRace', 'gender'].includes(field.index)) {
       this.sheetModifiedByPlayer.height = '';
       this.sheetModifiedByPlayer.weight = '';
       this.sheetModifiedByPlayer.heightModifierRolled = '';
     }
-    if (['characterRace', 'characterClass'].includes(field.name)) {
+    if (['characterRace', 'characterClass'].includes(field.index)) {
       this.sheetModifiedByPlayer.age = '';
     }
   }
 
-  receiveStatsFrom(fromObs$: Observable<StatisticDetails[]>) {
-    fromObs$.pipe(
-      takeUntilDestroyed(this.destroyRef),
-    ).subscribe((stats: StatisticDetails[]) => {
-      this.sheetModifiedByPlayer["stats"] = stats;
-      this.sheetModifiedListener$.next(this.sheetModifiedByPlayer);
-    });
+  receiveBasicField(field: BasicField): void {
+    this.controlField(field);
+    this.sheetModifiedByPlayer[field.index] = field.value;
+    this.updateSheetStream();
   }
 
-  receiveStatFrom(fromObs$: Observable<StatField>) {
-    fromObs$.pipe(
-      takeUntilDestroyed(this.destroyRef),
-    ).subscribe((statField: StatField) => {
-      this.sheetModifiedByPlayer["stats"][statField.index] = statField.stat;
-      this.sheetModifiedListener$.next(this.sheetModifiedByPlayer);
-    });
+  receiveStatListField(field: StatListField) {
+    const statList: StatisticDetails[] = field.value;
+    this.sheetModifiedByPlayer["stats"] = statList;
+    this.updateSheetStream();
   }
 
-  receiveSkillFrom(fromObs$: Observable<SkillDetails>) {
-    fromObs$.pipe(
-      takeUntilDestroyed(this.destroyRef),
-    ).subscribe((skill: SkillDetails) => {
-      this.sheetModifiedByPlayer['skills'][skill.id] = { rank: skill.ranks, complement: skill.complement };
-      this.sheetModifiedListener$.next(this.sheetModifiedByPlayer);
-    })
+  receiveStatField(statField: StatField) {
+    this.sheetModifiedByPlayer["stats"][statField.index] = statField.value;
+    this.updateSheetStream()
+  }
+
+  receiveSkillField(field: SkillField) {
+    this.sheetModifiedByPlayer['skills'][field.value.id] =
+    {
+      rank: field.value.ranks,
+      complement: field.value.complement
+    };
+    this.updateSheetStream();
+  }
+
+  updateSheetStream() {
+    this.sheetModifiedListener$.next(this.sheetModifiedByPlayer);
   }
 }
