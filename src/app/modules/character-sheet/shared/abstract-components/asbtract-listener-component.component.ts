bb0fc2b4df4666780f7abf4d7e5ba971a2c76cf0
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-18 17:22:53 +0200

    refactor: delete bad named file

--- src/app/modules/character-sheet/shared/abstract-components/asbtract-listener-component.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-import { ListenPlayerActionService } from "../services/listen-player-action.service";
-
-@Component({ template: "" })
-export abstract class AbstractListenerComponent {
-    constructor(protected listener: ListenPlayerActionService) { }
-}
