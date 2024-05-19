3ccc93df7848147db50d5c9a185ff7520031c07b
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-19 21:07:08 +0200

    feat: add an abstract component for watching dom changes'

---
+++ src/app/modules/character-sheet/shared/abstract-components/abstract-metamorphosis-component.component.ts
@@ -0,0 +1,29 @@
+import { Component, DestroyRef } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { interval, takeWhile } from "rxjs";
+
+@Component({ template: "" })
+export abstract class AbstractMetamorphosisComponent {
+
+    isbgred: boolean = false;
+
+    constructor(private destroyRef: DestroyRef) { }
+
+    handleDomChange(event: any) {
+        console.log(event.target.classList)
+        this.isbgred = true;
+        this.runExplosion();
+    }
+
+    runExplosion() {
+        const source = interval(3000);
+        source
+            .pipe(
+                takeUntilDestroyed(this.destroyRef),
+                takeWhile(() => this.isbgred),
+            )
+            .subscribe((n) => {
+                this.isbgred = false;
+            });
+    }
+}
