ecb1b8357cd6d5dd0ce408eb177aa53c08831879
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-20 16:48:37 +0200

    feat: add saving throws service

--- src/app/modules/character-sheet/shared/services/saving-throws.service.ts
+++ src/app/modules/character-sheet/shared/services/saving-throws.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { SavingThrowType } from '../../models/enums/saving-throws-type.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SavingThrowsService {
   constructor() { }
+
+  static getValue(level: number, savingType: SavingThrowType) {
+    if (savingType == SavingThrowType.low) {
+      return this.getLowValue(level);
+    } else {
+      return this.getHighValue(level);
+    }
+  }
+
+  private static getLowValue(level: number) {
+    return Math.floor(level / 3);
+  }
+
+  private static getHighValue(level: number) {
+    return Math.floor(level / 2) + 2;
+  }
 }
