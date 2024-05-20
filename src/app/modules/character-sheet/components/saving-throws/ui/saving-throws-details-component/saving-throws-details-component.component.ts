c527498236fa3864b9e1ace332cbefe114c73064
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-20 11:51:10 +0200

    feat: add saving throws components

---
+++ src/app/modules/character-sheet/components/saving-throws/ui/saving-throws-details-component/saving-throws-details-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saving-throws-details-component',
+  templateUrl: './saving-throws-details-component.component.html',
+  styleUrl: './saving-throws-details-component.component.scss'
+})
+export class SavingThrowsDetailsComponentComponent {
+
+}
---
+++ src/app/modules/character-sheet/components/saving-throws/ui/saving-throws-list-component/saving-throws-list-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saving-throws-list-component',
+  templateUrl: './saving-throws-list-component.component.html',
+  styleUrl: './saving-throws-list-component.component.scss'
+})
+export class SavingThrowsListComponentComponent {
+
+}
