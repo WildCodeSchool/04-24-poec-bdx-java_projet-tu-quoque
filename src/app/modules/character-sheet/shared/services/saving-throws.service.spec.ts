f2aca50f6ec73e811ac5dedeae2cb8b01606ce57
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-20 16:49:05 +0200

    test: add a test file for saving throws service

--- src/app/modules/character-sheet/shared/services/saving-throws.service.spec.ts
+++ src/app/modules/character-sheet/shared/services/saving-throws.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SavingThrowsService } from './saving-throws.service';
+import { SavingThrowType } from '../../models/enums/saving-throws-type.enum';
 
 describe('SavingThrowsService', () => {
   let service: SavingThrowsService;
@@ -13,4 +14,17 @@ describe('SavingThrowsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it("should be 5 for high saving throw and level 6", () => {
+    expect(SavingThrowsService.getValue(6, SavingThrowType.high)).toBe(5);
+  })
+  it("should be 11 for high saving throw and level 19", () => {
+    expect(SavingThrowsService.getValue(19, SavingThrowType.high)).toBe(11);
+  })
+  it("should be 2 for low saving throw and level 6", () => {
+    expect(SavingThrowsService.getValue(6, SavingThrowType.low)).toBe(2);
+  })
+  it("should be 6 for low saving throw and level 20", () => {
+    expect(SavingThrowsService.getValue(20, SavingThrowType.low)).toBe(6);
+  })
 });
