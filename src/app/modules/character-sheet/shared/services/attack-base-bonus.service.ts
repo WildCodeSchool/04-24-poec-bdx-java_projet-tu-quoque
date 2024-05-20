cdff30a64cd24697812c107724648c1f2eb14a19
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-20 18:56:26 +0200

    style: remove unused code

--- src/app/modules/character-sheet/shared/services/attack-base-bonus.service.ts
+++ src/app/modules/character-sheet/shared/services/attack-base-bonus.service.ts
@@ -6,34 +6,9 @@ import { AttackBaseBonusType } from '../../models/enums/attack-base-bonus-type.e
 })
 export class AttackBaseBonusService {
 
-  constructor() { }
-  // static getValue(level: number, bbaType: AttackBaseBonusType) {
-  //   if (bbaType == AttackBaseBonusType.low) {
-  //     return this.getLowValue(level);
-  //   } else if (bbaType == AttackBaseBonusType.medium) {
-  //     return this.getMediumValue(level);
-  //   } else {
-  //     return this.getHighValue(level);
-  //   }
-  // }
-
   static getValue(level: number, bbaType: AttackBaseBonusType) {
     const bba = Math.floor(level * bbaType);
     return this.getAllAttacks(bba);
-  }
-
-  private static getLowValue(level: number) {
-    let bba = Math.floor(level / 2);
-    return this.getAllAttacks(bba);
-  }
-
-  private static getMediumValue(level: number) {
-    let bba = Math.floor(level * 3 / 4);
-    return this.getAllAttacks(bba);
-  }
-
-  private static getHighValue(level: number) {
-    return this.getAllAttacks(level);
   }
 
   private static getAllAttacks(bba: number) {
