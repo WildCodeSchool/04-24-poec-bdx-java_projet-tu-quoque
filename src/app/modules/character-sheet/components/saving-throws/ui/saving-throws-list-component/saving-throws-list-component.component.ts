434eed27a0cf8ab90e11f72d4a068cc49a0c6586
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-21 10:34:07 +0200

    feat: get saving throw from parent and display it

--- src/app/modules/character-sheet/components/saving-throws/ui/saving-throws-list-component/saving-throws-list-component.component.ts
+++ src/app/modules/character-sheet/components/saving-throws/ui/saving-throws-list-component/saving-throws-list-component.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CharacterSavingThrows } from '../../../../models/classes/character-saving-throws.class';
 
 @Component({
   selector: 'app-saving-throws-list-component',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './saving-throws-list-component.component.scss'
 })
 export class SavingThrowsListComponentComponent {
-
+  @Input()
+  savingThrows$!: Observable<CharacterSavingThrows>;
 }
