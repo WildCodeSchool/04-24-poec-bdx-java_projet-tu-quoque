ed24e121dcb9ddaaab2b07aec900d12d2eb2049c
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-23 08:50:18 +0200

    style: add type return on methods and remove unused imports

--- src/app/modules/character-sheet/shared/services/attack-base-bonus.service.ts
+++ src/app/modules/character-sheet/shared/services/attack-base-bonus.service.ts
@@ -6,17 +6,16 @@ import { AttackBaseBonusType } from '../../models/enums/attack-base-bonus-type.e
 })
 export class AttackBaseBonusService {
 
-  static getValue(level: number, bbaType: AttackBaseBonusType) {
+  static getValue(level: number, bbaType: AttackBaseBonusType): number[] {
     const bba = Math.floor(level * bbaType);
     return this.getAllAttacks(bba);
   }
 
-  private static getAllAttacks(bba: number) {
+  private static getAllAttacks(bba: number): number[] {
     const attacks = [bba];
     while ((bba -= 5) > 0) {
       attacks.push(bba);
     }
     return attacks;
   }
-
 }
