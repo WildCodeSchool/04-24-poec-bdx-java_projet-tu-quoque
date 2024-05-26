a2dfe804a39e8cbbaa509cf7f7ba944963206d44
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-26 19:55:37 +0200

    refactor: rename weapons service

--- src/app/modules/character-sheet/shared/services/class-weapons.service.ts
+++ src/app/modules/character-sheet/shared/services/class-weapons.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { DbService } from '../../../shared/services/db-service/db.service';
-import { map, shareReplay, switchMap } from 'rxjs';
+import { map, switchMap } from 'rxjs';
 import { CharacterSheetService } from './character-sheet.service';
 import { CharacterClass } from '../../models/types/character-class.type';
-import { Weapon } from '../../models/types/weapons/weapon.type';
+import { WeaponDetails } from '../../models/types/weapons/weapon.type';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WeaponsService {
+export class ClassWeaponsService {
 
   constructor(
     private dbService: DbService,
@@ -16,23 +16,21 @@ export class WeaponsService {
   ) { }
 
   getWeapons$() {
-    return this.dbService.getWeapons$().pipe(
-      shareReplay()
-    );
+    return this.dbService.getWeapons$();
   }
 
   getClassWeapons$() {
     return this.sheet.getClasseDetails$().pipe(
       map((classDetail: CharacterClass) => classDetail ? classDetail.weapons : []),
       switchMap((weaponsAllowed: string[]) => this.getWeapons$().pipe(
-        map((weaponsList: Weapon[]) => this.searchInWeaponsFromDb(weaponsAllowed, weaponsList)),
+        map((weaponsList: WeaponDetails[]) => this.searchInWeaponsFromDb(weaponsAllowed, weaponsList)),
       )),
     )
   }
 
-  searchInWeaponsFromDb(weaponNameList: string[], weaponListFromDb: Weapon[]) {
+  searchInWeaponsFromDb(weaponNameList: string[], weaponListFromDb: WeaponDetails[]) {
     return weaponListFromDb.filter(
-      (weapon: Weapon) =>
+      (weapon: WeaponDetails) =>
         weaponNameList.includes(weapon.name) || weaponNameList.includes(weapon.category)
     );
   }
