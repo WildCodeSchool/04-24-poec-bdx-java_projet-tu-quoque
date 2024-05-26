ad1e8ccaaa95ed18af5ded787a4f1a7d8055be1f
Nicolas Leroux <hertford.fr@gmail.com>
2024-05-26 19:58:28 +0200

    feat: add method to update purse observable and manage new fields in listenPlayerService

--- src/app/modules/character-sheet/shared/services/purse.service.ts
+++ src/app/modules/character-sheet/shared/services/purse.service.ts
@@ -3,6 +3,8 @@ import { InitialPurseService } from './initial-purse.service';
 import { Observable, Subject } from 'rxjs';
 import { Purse } from '../../models/classes/purse-related/purse.class';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ListenPlayerActionService } from './listen-player-action.service';
+import { PurseField } from '../models/types/purse-field.type';
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +13,31 @@ export class PurseService {
   initialPurse$ = inject(InitialPurseService).getPurse$();
   purse$: Subject<Purse> = new Subject();
   destroyRef = inject(DestroyRef);
+  listener = inject(ListenPlayerActionService);
+  purseField$: Subject<PurseField> = new Subject();
 
   constructor() {
     this.init();
+    this.listener.receiveFieldFrom(this.purseField$);
   }
 
   init(): void {
     this.initialPurse$.pipe(
       takeUntilDestroyed(this.destroyRef),
-    ).subscribe((purse: Purse) => this.purse$.next(purse));
+    ).subscribe((purse: Purse) => this.updatePurse(purse));
   }
 
   getPurse$(): Observable<Purse> {
     return this.purse$.asObservable();
   }
+
+  updatePurse(purse: Purse) {
+    this.purse$.next(purse);
+    const field = {
+      index: "purse",
+      value: purse
+    };
+    this.purseField$.next(field);
+  }
+
 }
