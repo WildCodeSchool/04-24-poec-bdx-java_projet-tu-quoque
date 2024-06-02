e263cf30ac3c39f89883eb0b590942e217297e58
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-02 10:40:01 +0200

    style: folder creation for cleaning, format for cleaning

--- src/app/modules/dice-roll/components/features/dice-roll-page/dice-roll-page.component.ts
+++ src/app/modules/dice-roll/components/features/dice-roll-page/dice-roll-page.component.ts
@@ -4,36 +4,34 @@ import DiceBox from '@3d-dice/dice-box';
 @Component({
   selector: 'app-dice-roll-page',
   templateUrl: './dice-roll-page.component.html',
-  styleUrl: './dice-roll-page.component.scss'
+  styleUrl: './dice-roll-page.component.scss',
 })
 export class DiceRollPageComponent {
-
   diceBox: any;
-  inputText:string ="";
+  inputText: string = '';
 
-  @ViewChild('diceBoxContainer', { static: true }) diceBoxContainer!: ElementRef<HTMLDivElement>;
-  
+  @ViewChild('diceBoxContainer', { static: true })
+  diceBoxContainer!: ElementRef<HTMLDivElement>;
+
   ngOnInit() {}
 
   ngAfterViewInit(): void {
-      const containerId = '#diceBoxContainer';
-      this.diceBox = new DiceBox(containerId, {
-        id: 'diceBox',
-        assetPath: '/assets/',
-        theme:'default',
-        Offscreen: true,
-        scale: 5
-      });
+    const containerId = '#diceBoxContainer';
+    this.diceBox = new DiceBox(containerId, {
+      id: 'diceBox',
+      assetPath: '/assets/dice-box/',
+      theme: 'default',
+      Offscreen: true,
+      scale: 5,
+    });
 
-
-      this.diceBox.canvas.width = "400"
-      this.diceBox.canvas.height = "600"
-      this.diceBox.init()
-
+    this.diceBox.canvas.width = '400';
+    this.diceBox.canvas.height = '600';
+    this.diceBox.init();
+  }
+  rollDice(): void {
+    if (this.diceBox) {
+      this.diceBox.roll(this.inputText.split(' '));
     }
-    rollDice():void {
-      if(this.diceBox) {
-        this.diceBox.roll(this.inputText.split(" "))
-      }
-    }
+  }
 }
