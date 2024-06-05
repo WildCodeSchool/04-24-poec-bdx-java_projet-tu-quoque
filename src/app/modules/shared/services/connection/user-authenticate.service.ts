0c22c00101766a435efd813d0fafcc155048045a
Nelia <nelia.courtais@live.fr>
2024-06-05 14:15:25 +0200

    feat: fix connexion & register - add observable

--- src/app/modules/shared/services/connection/user-authenticate.service.ts
+++ src/app/modules/shared/services/connection/user-authenticate.service.ts
@@ -5,32 +5,27 @@ import { AuthResponse } from '../../models/types/users/auth-response';
 import { UserAuth } from '../../models/class/user-auth.model';
 import { TokenService } from './token.service';
 import { TokenResponse } from '../../models/class/token.model';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserAuthenticateService implements OnDestroy{
+export class UserAuthenticateService {
 
   private readonly _BASE_URL: string = environment.baseUrl + environment.auth.authenticate;
-  private subscription!: Subscription;
 
   constructor(
     private _http : HttpClient,
     private tokenService: TokenService
   ) { }
 
-  authenticateUser(userAuth: UserAuth): void {
+  authenticateUser(userAuth: UserAuth): Observable<TokenResponse> {
     this.tokenService.resetToken();
-    this.subscription = this._http.post<AuthResponse>(this._BASE_URL, userAuth)
-      .subscribe((tokenFromDB: TokenResponse) => {
+    return this._http.post<AuthResponse>(this._BASE_URL, userAuth).pipe(
+      map((tokenFromDB: TokenResponse) => {
         this.tokenService.updateToken(tokenFromDB);
+        return tokenFromDB;
       })
-  }
-
-  ngOnDestroy(): void {
-      if(this.subscription) {
-        this.subscription.unsubscribe();
-      }
+    );
   }
 }
