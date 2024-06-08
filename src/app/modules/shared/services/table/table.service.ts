79946011e11385290d3c717e84cc232b82ede33d
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-08 22:49:56 +0200

    style & refacto : remove space and useless imports + rename methods

--- src/app/modules/shared/services/table/table.service.ts
+++ src/app/modules/shared/services/table/table.service.ts
@@ -1,10 +1,7 @@
 import { Injectable, inject } from '@angular/core';
-import { Observable, map, switchMap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Table } from '../../models/types/users/table.type';
-import { ConnectionService } from '../connection/connection.service';
-import { UserBasicInfos } from '../../models/types/users/user-basic-infos.type';
 import { ApiRessourceService } from '../api-ressource/api-ressource.service';
-import { UserInfos } from '../../models/types/users/user-infos';
 import { environment } from '../../../../../environments/environment.development';
 import { HttpHeaders } from '@angular/common/http';
 import { LocalStorageService } from '../connection/local-storage.service';
@@ -13,7 +10,7 @@ import { LocalStorageService } from '../connection/local-storage.service';
   providedIn: 'root',
 })
 export class TableService extends ApiRessourceService<Table> {
-  
+
   private _localStorageService = inject(LocalStorageService);
   private readonly _BASE_URL: string = environment.baseUrl + '/tables';
 
