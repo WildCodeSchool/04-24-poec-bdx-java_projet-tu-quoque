d78238225bf62d520a056e7f78ad4540f87d4d0b
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-08 20:42:21 +0200

    feature: user-notes page & user-note-creation done

--- src/app/modules/notepad/modules/user-notepad/modules/user-note-management/features/user-notes/user-notes.component.ts
+++ src/app/modules/notepad/modules/user-notepad/modules/user-note-management/features/user-notes/user-notes.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { NoteService } from '../../../../../../../shared/services/note/note.service';
 import { Note } from '../../../../../../../shared/models/types/users/note.type';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserInfos } from '../../../../../../../shared/models/types/users/user-infos';
 import { ConnectionService } from '../../../../../../../shared/services/connection/connection.service';
 
@@ -12,15 +12,18 @@ import { ConnectionService } from '../../../../../../../shared/services/connecti
   styleUrl: './user-notes.component.scss',
 })
 export class UserNotesComponent {
-
+  
   user!: UserInfos;
+  role: String = 'user';
 
-  constructor(
-    private _route: ActivatedRoute,
-  ) {}
+  constructor(private _route: ActivatedRoute, private _router: Router) {}
 
   ngOnInit(): void {
     const userData = this._route.snapshot.data['user'];
-    this.user = userData; 
+    this.user = userData;
+  }
+
+  onClick() {
+    this._router.navigateByUrl(`notepad/user/notes/creation/${this.role}`);
   }
 }
--- src/app/modules/shared/services/table/table.service.ts
+++ src/app/modules/shared/services/table/table.service.ts
@@ -14,7 +14,7 @@ import { LocalStorageService } from '../connection/local-storage.service';
 })
 export class TableService extends ApiRessourceService<Table> {
   
-  private _localStorageService = inject(LocalStorageService)
+  private _localStorageService = inject(LocalStorageService);
   private readonly _BASE_URL: string = environment.baseUrl + '/tables';
 
   override getRessourceUrl(): string {
@@ -24,10 +24,8 @@ export class TableService extends ApiRessourceService<Table> {
   getUserTableByIdNew(id: number): Observable<any> {
     const token = this._localStorageService.getToken();
     const headers = new HttpHeaders({
-      "Authorization": 
-      `Bearer ${token}`
-      })
-     return this._http.get(this._BASE_URL + `/get/${id}`, {headers})
+      Authorization: `Bearer ${token}`,
+    });
+    return this._http.get(this._BASE_URL + `/get/${id}`, { headers });
   }
-
 }
