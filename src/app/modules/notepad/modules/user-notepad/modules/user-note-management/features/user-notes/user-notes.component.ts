80c49c65341452afda5137af75ca71fe76ed734f
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-08 17:14:10 +0200

    refacto: user-infos type updated & user-notes page adapted to back response

--- src/app/modules/notepad/modules/user-notepad/modules/user-note-management/features/user-notes/user-notes.component.ts
+++ src/app/modules/notepad/modules/user-notepad/modules/user-note-management/features/user-notes/user-notes.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { NoteService } from '../../../../../../../shared/services/note/note.service';
 import { Note } from '../../../../../../../shared/models/types/users/note.type';
+import { ActivatedRoute } from '@angular/router';
+import { UserInfos } from '../../../../../../../shared/models/types/users/user-infos';
+import { ConnectionService } from '../../../../../../../shared/services/connection/connection.service';
 
 @Component({
   selector: 'app-user-notes',
@@ -9,10 +12,17 @@ import { Note } from '../../../../../../../shared/models/types/users/note.type';
   styleUrl: './user-notes.component.scss',
 })
 export class UserNotesComponent {
-  
-  noteList$: Observable<Note[]> = this._notesService.getNoteListByUser();
+
+  user!: UserInfos;
 
   constructor(
     private _notesService: NoteService,
+    private _route: ActivatedRoute,
+    private _connectionService: ConnectionService
   ) {}
+
+  ngOnInit(): void {
+    const userData = this._route.snapshot.data['user'];
+    this.user = userData; 
+  }
 }
