c1b81bb24801d0d928f885f3c1dbd7bbe86d8e9f
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-10 21:57:06 +0200

    feat: header icon adaptable

--- src/app/modules/navigation/components/features/header/header.component.ts
+++ src/app/modules/navigation/components/features/header/header.component.ts
@@ -2,6 +2,10 @@ import { Component, inject } from '@angular/core';
 import { NavigationService } from '../../../../shared/services/navigation.service';
 import { Router } from '@angular/router';
 import { ConnectionService } from '../../../../shared/services/connection/connection.service';
+import { Observable, forkJoin, map } from 'rxjs';
+import { CharacterFullDTO } from '../../../../shared/models/types/users/character-full-dto';
+import { GameTableFullDTO } from '../../../../shared/models/types/users/table-full-dto';
+import { UserInfos } from '../../../../shared/models/types/users/user-infos';
 
 @Component({
   selector: 'app-header',
@@ -9,21 +13,32 @@ import { ConnectionService } from '../../../../shared/services/connection/connec
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  
   title: string = 'tu quoque';
 
-  private _navigationService = inject(NavigationService)
-  private _connectionService = inject(ConnectionService)
-  private _router = inject(Router)
+  private _navigationService = inject(NavigationService);
+  private _connectionService = inject(ConnectionService);
+  private _router = inject(Router);
 
-  userConnected$ = this._connectionService.getUserConnected$()
-  userHomeLink: string = '/user/home'
-  visitorHomeLink: string ='/visitor/home'
+  userConnected$: Observable<UserInfos | null> = this._connectionService.getUserConnected$();
+  characterConnected$: Observable<CharacterFullDTO | null> = this._connectionService.getCharacterConnectedNew$();
+  tableConnected$: Observable<GameTableFullDTO | null> = this._connectionService.getTableConnectedNew$();
+  userHomeLink: string = '/user/home';
+  visitorHomeLink: string = '/visitor/home';
   urlToGoBack!: string;
+  avatar$: Observable<string |undefined> = forkJoin([this.userConnected$, this.characterConnected$, this.tableConnected$]).pipe(
+    map(([userConnected, characterConnected, tableConnected]) => {
+      if (characterConnected) {
+        return characterConnected.avatar;
+      } else if (tableConnected) {
+        return tableConnected.Avatar;
+      } else {
+        return userConnected?.avatar;
+      }
+    }))
 
   goToNotepad(): void {
     localStorage.setItem('routeToGoBack', this._router.url);
-    this._router.navigateByUrl('notepad')
+    this._router.navigateByUrl('notepad');
   }
   onClick() {
     window.scrollTo(0, 0);
@@ -31,6 +46,6 @@ export class HeaderComponent {
   }
 
   goToDiceRoll() {
-    this._router.navigateByUrl('dice-roll')
+    this._router.navigateByUrl('dice-roll');
   }
 }
