35febe3f9f54491f3d54728895d4efefb24e29fe
Nelia <nelia.courtais@live.fr>
2024-06-10 13:38:05 +0200

    feat: add guards

--- src/app/modules/shared/guards/auth.guard.ts
+++ src/app/modules/shared/guards/auth.guard.ts
@@ -1,20 +1,17 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { TokenService } from '../services/connection/token.service';
-import { Router } from 'react-router-dom';
-import { of } from 'rxjs';
+
 
 export const authGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
 ) => {
-  console.log("toto");
   
   const tokenService = inject(TokenService);
     const router = inject(Router);
   
     const token = tokenService.getTokenFromLocalStorageAndDecode();
-  
     if (token) {
       return true;
     } else {
