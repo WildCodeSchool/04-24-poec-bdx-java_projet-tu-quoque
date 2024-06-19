1f8c6285af5127c50097398ccb9ede6bae6ec631
bfsylvain <sylvainbonnaure@hotmail.com>
2024-06-19 21:15:05 +0200

    feat: user-charcters page working on delete

--- src/app/modules/user/modules/character/modules/character-management/components/features/user-characters/user-characters.component.ts
+++ src/app/modules/user/modules/character/modules/character-management/components/features/user-characters/user-characters.component.ts
@@ -5,6 +5,7 @@ import { Character } from '../../../../../../../../shared/models/types/users/cha
 import { UserInfos } from '../../../../../../../../shared/models/types/users/user-infos';
 import { ActivatedRoute } from '@angular/router';
 import { ConnectionService } from '../../../../../../../../shared/services/connection/connection.service';
+import { CharacterDTO } from '../../../../../../../../shared/models/types/users/character-dto';
 
 @Component({
   selector: 'app-user-characters',
@@ -14,7 +15,6 @@ import { ConnectionService } from '../../../../../../../../shared/services/conne
 export class UserCharactersComponent {
   userCharacterList$: Observable<Character[]> | null = null;
   user: UserInfos | null = null;
-
   constructor(
     private _characterService: CharacterService,
     private _route: ActivatedRoute,
@@ -24,5 +24,27 @@ export class UserCharactersComponent {
   ngOnInit(): void {
     const userData = this._route.snapshot.data['user'];
     this.user = userData;
+    // this._connectionService.setUserConnected(userData) --> set le user dans le service donc devoir aussi :
+    // this._connectionService.setUserCharacterList(userData.playerCharacterList) nouveau behavior subject a rajouter dans le service
+    // ici on prendra plutot le behavior subjet useCharacterList pour display dans le html
+    //
   }
+
+  // addElement(characterId: number): void {
+  //   if(this.user?.playerCharacterList) {
+  //     const characterList = this.user.playerCharacterList
+  //     // this.user.playerCharacterList = [...this.user.playerCharacterList, {id:9, name: "bonobo"} ] --> pour ajouter un character
+  //     //quand on clique sur un personnage ça le supprime, meme logique
+  //     // décaler la logique dans le service donc:
+  //     // créer une méthode pour next la
+
+  //     const characterListUpdated = characterList.filter(
+  //       (character: CharacterDTO) => character.id !== characterId
+  //     );
+  //     this.characterList$.next(characterList);
+  //     return this._http.delete(this._BASE_URL + `/delete/${eventId}`, {
+  //       headers,
+  //     });
+  //   }
+  // }
 }
