ee80e0676ffb743efddd036f27c48adfcf3e696a
Nelia <nelia.courtais@live.fr>
2024-06-24 10:47:21 +0200

    feat: add redirection when token expired

--- src/app/modules/shared/services/uploadFile/upload-file.service.ts
+++ src/app/modules/shared/services/uploadFile/upload-file.service.ts
@@ -8,8 +8,13 @@ export class UploadFileService {
   private selectedFileSubject = new BehaviorSubject<File | null>(null);
   selectedFile$ = this.selectedFileSubject.asObservable();
 
-  setSelectedFile(file: File | null) : void {
+  setSelectedFile(file: File | null): void {
     this.selectedFileSubject.next(file);
+    if (file) {
+      console.log(`Selected file set: ${file.name}`);
+    } else {
+      console.log('Selected file cleared');
+    }
   }
 
   getSelectedFile(): File | null {
